1358f8d1802a18432e15b99a26e2962d6c10781f
Michal <michal.rosicki@gmail.com>
2020-04-25 21:15:12 +0200

    search by nutrients

---
+++ src/components/RecipiesSearchByNutrients/RecipeNutrientsComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="RecipeComponent">
+    <b-card v-bind:title="title"
+            v-bind:img-src="imageUrl"
+            img-alt="Image"
+            img-top
+            class="mb-1 mr-1"
+            style="min-width: 15rem; max-width: 20rem; height: 37rem" img-height="300rem">
+      <!--      <b-img :src="imageUrl" center fluid rounded="10"/>-->
+      <b-card-text>
+        <p class="information">Calories: {{calories}}</p>
+        <p class="information">Carbs: {{carbs}}</p>
+        <p class="information">Fat: {{fat}}</p>
+        <p class="information">Protein: {{protein}}</p>
+      </b-card-text>
+      <b-button href="#" variant="primary" style="color: aliceblue">Go to recipe</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+  import {baseRecipiesApiPhotosUrl} from '../../shared/constants'
+
+  export default {
+    data() {
+      return {
+        results: []
+      }
+    },
+    props: {
+      id: Number,
+      title: String,
+      image: String,
+      imageUrls: Array,
+      calories: Number,
+      carbs: String,
+      fat: String,
+      protein: String
+    },
+    computed: {
+      imageUrl: function () {
+        return `${this.image}`
+      }
+    },
+    mounted() {
+    },
+    created() {
+    },
+    methods: {
+      print: function () {
+        console.log(this.id)
+      }
+    }
+  }
+</script>
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Recipes from '../components/RecipesSearchGeneral/RecipiesComponent';
 import RecipesByIngredient from '../components/RecipiesSearchByIngredients/RecipesSearchByIngredients';
 import GenerateMealPlan from '../components/GenerateMealPlan/GenerateMealPlan';
+import RecipesByNutrient from '../components/RecipiesSearchByNutrients/RecipiesSearchByNutrientsComponent';
 
 Vue.use(Router);
 
@@ -12,6 +13,7 @@ export default new Router({
   routes: [
     {path: '/', name: 'Recipes', component: Recipes},
     {path: '/by-ingredients', name: 'RecipesByIngredients', component: RecipesByIngredient},
+    {path: '/by-nutrients', name: 'RecipiesByNutrients', component: RecipesByNutrient},
     {path: '/generate-meal-plan', name: 'RecipesByIngredients', component: GenerateMealPlan}
   ]
 });
