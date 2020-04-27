40f392b3c5eedb1e7070954c88584028ee5d350e
Nalepsky <nalepka.marek@student.pk.edu.pl>
2020-04-27 03:32:26 +0200

    #18 add modal with recipe details

--- src/components/RecipesSearchGeneral/RecipeComponent.vue
+++ src/components/RecipesSearchGeneral/RecipeComponent.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="RecipeComponent">
+    <b-modal ref="recipe-modal" size="lg" centered hide-footer :title="recipeTitle">
+      <div>
+        <RecipeDetailComponent
+          v-bind:id=this.id
+          v-bind:units=this.units
+          v-bind:steps=this.steps
+          v-bind:ingredients=this.ingredients
+          v-bind:image=this.image
+        />
+        <b-button variant="outline-info" block @click="changeUnits">{{changeUnitButtonName}}</b-button>
+        <b-button variant="outline-danger" block @click="hideModal">Close</b-button>
+      </div>
+    </b-modal>
     <b-card v-bind:title="title"
             v-bind:img-src="imageUrl"
             img-alt="Image"
@@ -12,20 +25,27 @@
         <p class="information">Servings: {{servings}}</p>
         <p class="information">Likes: {{likes}} <button v-on:click="setLikes(id, likes+1)">Add Like</button></p>
       </b-card-text>
-      <b-button href="#" variant="primary" style="color: aliceblue">Go to recipe</b-button>
+      <b-button href="#" variant="primary" style="color: aliceblue" v-on:click="showRecipe">Go to recipe</b-button>
     </b-card>
   </div>
 </template>
 
 <script>
-  import {baseRecipiesApiPhotosUrl} from '../../shared/constants';
+  import {baseRecipiesApiPhotosUrl, getWithEndpoint, baseRecipiesApiUrl} from '../../shared/constants';
   import {getLikes, setLikes} from '../../shared/DBHandling';
-
+  import RecipeDetailComponent from "./RecipeDetailComponent.vue";
+  import axios from "axios";
   export default {
+    components: {RecipeDetailComponent,},
     data() {
       return {
         results: [],
-        likes: 0
+        steps: [],
+        ingredients: [],
+        likes: 0,
+        recipeTitle: "",
+        units: "metric",
+        changeUnitButtonName: "change units to us"
       }
     },
     props: {
@@ -43,7 +63,7 @@
     },
     mounted() {
       getLikes(this.id)
-      .then(likes => this.likes = likes);
+        .then(likes => this.likes = likes);
     },
     created() {
     },
@@ -53,7 +73,37 @@
       },
       setLikes: function(id, likes){
         setLikes(id, likes)
-        .then(likes => this.likes = likes);
+          .then(likes => this.likes = likes);
+      },
+      showRecipe: function() {
+        axios
+          .get(
+            getWithEndpoint(
+              new URL(`${baseRecipiesApiUrl}/${this.id}/analyzedInstructions`)
+            )
+          )
+          .then(response => (this.steps = response.data[0].steps));
+        axios
+          .get(
+            getWithEndpoint(
+              new URL(`${baseRecipiesApiUrl}/${this.id}/ingredientWidget.json`)
+            )
+          )
+          .then(response => (this.ingredients = response.data.ingredients));
+        this.recipeTitle = this.title;
+        this.$refs["recipe-modal"].show();
+      },
+      hideModal() {
+        this.$refs["recipe-modal"].hide();
+      },
+      changeUnits() {
+        if (this.units === "metric") {
+          this.units = "us";
+          this.changeUnitButtonName = "change units to metric";
+        } else {
+          this.units = "metric";
+          this.changeUnitButtonName = "change units to us";
+        }
       }
     }
   }
---
+++ src/components/RecipesSearchGeneral/RecipeDetailComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="RecipeDetailComponent">
+    <div v-if="this.steps" class="d-block text-left">
+      <div style="display: flex; justify-content: space-between">
+        <div>
+          <h5>You will need:</h5>
+          <ul v-if="this.units === 'metric'" id="ingridients" style="list-style-type:none; margin-left: 1rem">
+            <li v-for="el in this.ingredients" :key="el">
+              {{ el.amount.metric.value }} {{ el.amount.metric.unit }} of
+              <b>{{ el.name }}</b>
+            </li>
+          </ul>
+          <ul v-if="this.units === 'us'" id="ingridients" style="list-style-type:none; margin-left: 1rem">
+            <li v-for="el in this.ingredients" :key="el">
+              {{ el.amount.us.value }} {{ el.amount.us.unit }} of
+              <b>{{ el.name }}</b>
+            </li>
+          </ul>
+        </div>
+        <img v-bind:src=imageUrl height="200rem" style="float: right; margin-top: 1rem; margin-right: 1rem">
+      </div>
+      <h5>All you have to do is:</h5>
+      <ul id="steps" style="list-style-type:none; margin-left: 1rem">
+        <li v-for="el in this.steps" :key="el">
+          <b>{{el.number}}</b>
+          : {{ el.step }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {baseRecipiesApiPhotosUrl} from '../../shared/constants';
+  export default {
+    data() {
+      return {};
+    },
+    props: {
+      id: Number,
+      units: String,
+      steps: Array,
+      ingredients: Array,
+      image: String,
+    },
+    computed: {
+      imageUrl: function () {
+        return `${baseRecipiesApiPhotosUrl}${this.image}`
+      }
+    },
+    mounted(){},
+    methods: {}
+  };
+</script>
